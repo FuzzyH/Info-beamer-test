e7d63861a1b8aa32b67f23a2c782a3142b26ecdf
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-11 18:23:41 +0200

    [maybe]

--- node.lua
+++ node.lua
@@ -1,9 +1,6 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font("UniversforUniS55Rm-Regular.ttf")
-local json = require "json"
-local utils = require(api.localized "utils")
-
 
 local jokes 
 util.json_watch("dadjokes.json", function (dadjokes)
@@ -11,6 +8,6 @@ util.json_watch("dadjokes.json", function (dadjokes)
 end)
 
 function node.render()
-    font:write(120, 320, ""..jokes.."", 100, 1,1,1,1)
+    font:write(120, 320, ""..jokes["joke"].."", 100, 1,1,1,1)
 end
 
