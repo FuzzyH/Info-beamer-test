f83e6179fddc10e6d2f0cf2c35801ed4edae52f5
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-11 12:49:55 +0200

    [fix] cycle through fonts

--- node.lua
+++ node.lua
@@ -1,6 +1,6 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
-local font = resource.load_font("UniversforUniS45LtObl-Rg.ttf")
+local font = resource.load_font("UniversforUniS55Rm-Regular.ttf")
 
 function node.render()
     font:write(120, 320, "Hello World", 100, 1,1,1,1)
