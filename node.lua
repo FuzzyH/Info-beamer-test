ef48835830c1128e96702e9dfe18f99e6dd7768e
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-11 12:48:12 +0200

    [add] font

--- node.lua
+++ node.lua
@@ -1,6 +1,6 @@
-gl.setup(1920, 1080)
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
-local font = resource.load_font("silkscreen.ttf")
+local font = resource.load_font("UniversforUniS45LtObl-Rg.ttf")
 
 function node.render()
     font:write(120, 320, "Hello World", 100, 1,1,1,1)
