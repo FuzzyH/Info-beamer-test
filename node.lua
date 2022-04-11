a6653386311c89667cfc7ebc9b0e33af33dd82ff
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-11 18:01:36 +0200

    [add] joke conection

--- node.lua
+++ node.lua
@@ -1,10 +1,15 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font("UniversforUniS55Rm-Regular.ttf")
+local json = require "json"
 
+local jokes 
+util.file_watch("dadjokes.json", function (content)
+    jokes = json.decode(content)
+end)
 
 function node.render()
 
-    font:write(120, 320, "no HTML :(", 100, 1,1,1,1)
+    font:write(120, 320, jokes, 100, 1,1,1,1)
 end
 
