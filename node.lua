3437a843ad3df6ffb74b4285f20340272c6182a6
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-11 18:29:02 +0200

    [test] lua logging

--- node.lua
+++ node.lua
@@ -2,12 +2,18 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local font = resource.load_font("UniversforUniS55Rm-Regular.ttf")
 
+local function log(system, format, ...)
+    return print(string.format("[%s] " .. format, system, ...))
+end
+
 local jokes 
 util.json_watch("dadjokes.json", function (dadjokes)
     jokes = dadjokes
 end)
 
 function node.render()
+    log("Renderer", "jokes jokes jokes")
+    log("Renderer", "jokes: "..jokes.."")
     font:write(120, 320, ""..jokes["joke"].."", 100, 1,1,1,1)
 end
 
