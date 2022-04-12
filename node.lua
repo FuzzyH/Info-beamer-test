e756274070071989cd8f15845b03c244f1e1f062
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-12 09:52:05 +0200

    [add] smthing annoyed

--- node.lua
+++ node.lua
@@ -3,19 +3,19 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 local font = resource.load_font("UniversforUniS55Rm-Regular.ttf")
 local json = require(json)
 
+local jokes
+util.file_watch("dadjokes.json", function (dadjokes)
+    jokes = dadjokes
+end)
+
 local function log(system, format, ...)
     return print(string.format("[%s] " .. format, system, ...))
 end
-
-local jokes
-util.file_watch("dadjokes.json", function (dadjokes)
-    jokes = json.decode(dadjokes)
-end)
 
 function node.render()
 
     log("Renderer", "Rendering")
     log("Renderer", "jokes: " ..jokes.."")
-    --font:write(120, 320, ""..jokes["0"]["joke"].."", 100, 1,1,1,1)
+    font:write(120, 320, "Test", 100, 1,1,1,1)
 end
 
