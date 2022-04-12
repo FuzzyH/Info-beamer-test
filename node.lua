ee7c52f2c2fb670f4eafa35b77cdf0f76c3fe671
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-12 12:59:37 +0200

    [rmv] wrapper in node.lua

--- node.lua
+++ node.lua
@@ -2,10 +2,9 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 -- include libarys
 local json = require "json"
-local utf8 = require "utf8"
 
 -- include other files
---local utility = require "utility"
+local utility = require "utility"
 local font = resource.load_font("UniversforUniS55Rm-Regular.ttf")
 
 -- variables:
@@ -26,56 +25,13 @@ end
 
 -- other functions
 
-local function wrap(str, font, size, max_w)
-    local lines = {}
-    local space_w = font:width(" ", size)
-
-    local remaining = max_w
-    local line = {}
-    local tokens = {}
-    for token in utf8.gmatch(str, "%S+") do
-        local w = font:width(token, size)
-        if w >= max_w then
-            while #token > 0 do
-                local cut = #token
-                for take = 1, #token do
-                    local sub_token = utf8.sub(token, 1, take)
-                    w = font:width(sub_token, size)
-                    if w >= max_w then
-                        cut = take-1
-                        break
-                    end
-                end
-                tokens[#tokens+1] = utf8.sub(token, 1, cut)
-                token = utf8.sub(token, cut+1)
-            end
-        else
-            tokens[#tokens+1] = token
-        end
-    end
-    for _, token in ipairs(tokens) do
-        local w = font:width(token, size)
-        if remaining - w < 0 then
-            lines[#lines+1] = table.concat(line, "")
-            line = {}
-            remaining = max_w
-        end
-        line[#line+1] = token
-        line[#line+1] = " "
-        remaining = remaining - w - space_w
-    end
-    if #line > 0 then
-        lines[#lines+1] = table.concat(line, "")
-    end
-    return lines
-end
 
 -- rendering
 function node.render()
     if jokes["joke"] ~= nil then
-        local font_size = 120
+        local font_size = 100
         -- log("Renderer", "before wrapper")
-        local lines = wrap(jokes["joke"], font, font_size, canvas_width)
+        local lines = utility.wrap(jokes["joke"], font, font_size, canvas_width)
         for idx = 1, #lines do
             local line = lines[idx]
             -- log("Renderer", "lines: " ..line)
