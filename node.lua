ae906aaa38b5cbc5598aaaed45ccc5fd0e2acc1c
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-13 11:02:19 +0200

    [GOD] I miss my debugger and toolchain....

--- node.lua
+++ node.lua
@@ -17,7 +17,6 @@ local font = resource.load_font("default-font.ttf")
 local font_size = 100
 local font_color = {1, 1, 1, 1}
 
-
 -- Watchers
 util.file_watch("dadjokes.json", function (dadjokes)
     jokes = json.decode(dadjokes)
@@ -28,7 +27,7 @@ local function log(system, format, ...)
     return print(string.format("[%s] " .. format, system, ...))
 end
 
--- updated config
+-- updated config by event
 node.event("config_updated", function(config)
     font_size = config.font_size
     font_color = config.color
@@ -37,16 +36,12 @@ end)
 -- other functions
 function draw_dadjoke(x1, y1, width, height)
     if jokes["joke"] ~= nil then
-        -- log("Renderer", "before wrapper")
         log("node.lua", "before wraper")
-        log("node.lua", "variables: "..font_size.."")
-        log("node.lua", "variables: ".. font..", "..font_size.."")
         local lines = utility.wrap(jokes["joke"], font, font_size, width)
         for idx = 1, #lines do
             local line = lines[idx]
             -- log("Renderer", "lines: " ..line)
-            font:write(x1, y1+idx*font_size+10, line, font_size, font_color)
-            font:write(300,300,"hw",1,1,1,1)
+            font:write(x1, y1+idx*font_size+border, line, font_size, font_color)
         end
     else
         log("node.lua", "Table is nil")
