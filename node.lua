deb349fc1829679622889f4bc011d51a2c9622d5
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-13 10:33:20 +0200

    [add] logging fix margin

--- node.lua
+++ node.lua
@@ -6,18 +6,16 @@ local json = require "json"
 -- include other files
 local utility = require "utility"
 
-
-
 -- variables:
 local jokes
 local border = math.floor(NATIVE_HEIGHT * 0.05)
 local canvas_width = NATIVE_WIDTH - border
 local canvas_height = NATIVE_HEIGHT - border
-local font_size = 100
-local font_color = {1, 1, 1, 1}
+
 -- font
 local font = resource.load_font("default-font.ttf")
-
+local font_size = 100
+local font_color = {1, 1, 1, 1}
 
 
 -- Watchers
@@ -40,15 +38,16 @@ end)
 function draw_dadjoke(x1, y1, width, height)
     if jokes["joke"] ~= nil then
         -- log("Renderer", "before wrapper")
+        log("node.lua", "before wraper")
         local lines = utility.wrap(jokes["joke"], font, font_size, width)
         for idx = 1, #lines do
             local line = lines[idx]
             -- log("Renderer", "lines: " ..line)
-            font:write(x1, y1+idx*font_size+margin, line, font_size, font_color)
+            font:write(x1, y1+idx*font_size+10, line, font_size, font_color)
             font:write(300,300,"hw",1,1,1,1)
         end
     else
-        log("Renderer", "Table is nil")
+        log("node.lua", "Table is nil")
     end
 end
 
