e8f4757e5e34c99d48b61d44ce58c51293db46b7
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-13 18:53:01 +0200

    [test] CONFIG.

--- node.lua
+++ node.lua
@@ -1,22 +1,23 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+util.init_hosted()
 
 -- include libarys
 local json = require "json"
 local utf8 = require "utf8"
 
 -- include other files
-local utility = require "utility"
+require "utility"
 
 -- variables:
 local jokes
 local border = math.floor(NATIVE_HEIGHT * 0.05)
 local canvas_width = NATIVE_WIDTH - border
 local canvas_height = NATIVE_HEIGHT - border
-local config
+
 
 -- font
 local font = resource.load_font("default-font.ttf")
-local font_size = 100
+local font_size = CONFIG.font_size
 local r, g, b = 1, 1, 1
 
 -- Watchers
@@ -29,14 +30,7 @@ local function log(system, format, ...)
     return print(string.format("[%s] " .. format, system, ...))
 end
 
--- updated config by event
-node.event("content_update", function(filename, file)
-    if filename == "config.json" then
-        config = json.decode(resource.load_file(file))
-        font_size = config.font_size
-        r,g,b = parse_rgb(config.color or "#ffffff")
-    end
-end)
+
 
 -- other functions
 function wrap(str, font, size, max_w)
