87f003cb127856b47adc8a0deb5d7f8ff6ef6584
ac123298 <ulf.kunze@tik.uni-stuttgart.de>
2022-04-15 10:13:34 +0200

    [try] canabalized tile

--- tile.lua
+++ tile.lua
@@ -1,13 +1,21 @@
 local api, CHILDS, CONTENTS = ...
 
-
+-- imports
+local utility = require(api.localized "utility")
 local anims = require(api.localized "anims")
+
+local json require "json"
 local utf8 = require "utf8"
+
+--- Variables:
+-- default font
 local font = resource.load_font("default-font.ttf")
 local font_size = 100
-local margin = 10
 local r,g,b = 1, 1, 1
+-- other
+local margin = 10
 local jokes
+
 
 M = {}
 
@@ -88,8 +96,7 @@ end
 
 function M.updated_config_json(config)
     print "config updated"
-    --font = resource.load_font(api.localized(config.font.asset_name))
-    font = resource.load_font("default-font.ttf")
+    font = resource.load_font(api.localized(config.font.asset_name))
     font_size = config.font_size
     r,g,b = parse_rgb(config.color)
 
@@ -97,10 +104,46 @@ end
 
 
 function M.task(starts, ends, config, x1, y1, x2, y2)
+    local boundingbox_height = y2-y1
+    local boundingbox_width = x2-x1
+
+    print("ACTUAL SCREEN SIZE " .. boundingbox_width .. "x" .. boundingbox_height)
+
+    api.wait_t(starts-2.5)
+
+    local a = anims.Area(boundingbox_width, boundingbox_height)
+    local S = starts
+    local E = ends
+
+    local lines = wrap(
+        jokes["joke"], font, font_size, boundingbox_width-2*margin
+    )
+
+    local function mk_content_box(x, y)                                              
+        y = y + margin
+        for idx = 1, #lines do
+            local line = lines[idx]
+            a.add(anims.moving_font(S, E, font, x+margin, y, line, font_size,
+                r, g, b, a
+            )); S=S+0.1; y=y+font_size
+        end
+    end
+
+    local text_height = #lines*font_size + 2*margin
+
+    print(boundingbox_width, boundingbox_height, text_height)
+
+    local text_y = math.min(
+        math.max(
+            font_size*1.6+3*margin,
+            130
+        ),
+        boundingbox_height-text_height
+    )
+    mk_content_box(0, text_y)
+
     for now in api.frame_between(starts, ends) do
-        api.screen.set_scissor(x1, y1, x2, y2)
-        draw_dadjoke(x1, y1, x2-x1, y2-y1)
-        api.screen.set_scissor()
+        a.draw(now, x1, y1, x2, y2)
     end
 end
 
